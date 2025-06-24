481ed60acd48ea2508f0c166aded486aa0ac27e5
Muhammad Ahad <muhmmadahad594@gmail.com>
2025-06-25 00:55:19 +0500

    sample viewing clients done

--- models/SampleViewingClients.ts
+++ models/SampleViewingClients.ts
@@ -11,6 +11,7 @@ export interface ISampleViewingClient extends Document {
     unit: string
     price: number
     status: 'pending' | 'accepted' | 'rejected'
+    shippingCost : number
   }[]
   viewingStatus: 'pending' | 'viewed'
   sentAt: Date
@@ -28,6 +29,7 @@ const SampleViewingClientSchema = new Schema({
       qty: { type: Number, required: true },
       unit: { type: String, required: true },
       price: { type: Number, required: true },
+      shippingCost : {type : Number, required : true},
       status: { type: String, enum: ['pending', 'accepted', 'rejected'], default: 'pending' },
     },
   ],
